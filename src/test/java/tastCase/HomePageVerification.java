9bf39abd69a402d9773fcad7041886580c19e994
mahfoozt <mahfooz.murshalin@gmail.com>
2024-04-23 10:00:08 -0700

    add new sysout

--- src/test/java/tastCase/HomePageVerification.java
+++ src/test/java/tastCase/HomePageVerification.java
@@ -10,7 +10,8 @@ public class HomePageVerification extends Base{
 		assertion("//h1[text()='Welcome to MicroTech NA.']","Welcome to MicroTech NA.");
 		
 		System.out.println("Hello gitHub");
-
+		System.out.println("Hello gitHub1");
+		System.out.println("Hello gitHub2");
 	}
 
 }
